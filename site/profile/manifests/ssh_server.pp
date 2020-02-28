fdf79b44c348a9f37b08d63789948115a813ce78
Gakus150 <47794155+Gakus150@users.noreply.github.com>
2020-02-28 11:13:49 -0600

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server{
                 ensure => present,
                 user => 'root',
                 type => 'ssh-rsa',
-                key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDMXtYvrkeipqnHBESu3L2vXfmSnjGev4VXx9fdLbST8IPS2Ano8MS/QXDcMKiZ8wyQ4N0xAZCnUGCkS/QFNeLIYmKZ6ZT61GEyQA7QSE7aq0XVOum1tOB5XTUvTdxUBJtHYOzIlevZzbFKAec+IbBwBSLBwYfy+AGCRvnZhBIdO2TLSJrGo1mEYVe2xqpyxXp9ICchTTE9XwFUdEFo/wGDs4Y8CseJdtwcQTuuYC3QLB+YYzWDq8FQfzFPsSmX47iQwfyf9sPU2TXLzX7Rr4mIgkdvLI5clF7N1SMzp4afEZV16A6iyJKErFhg7GE2kyEg4/MIckgoBmjeYLiRER4n',
+                key => '', 
         }
 }
